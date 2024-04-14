443a9dc952f19ad9e595a84657ef284339a9ad34
kunbolsyn <alisher.kunbolsyn@gmail.com>
2024-04-14 11:01:41 +0500

    add card update

--- frontend/src/Cards.js
+++ frontend/src/Cards.js
@@ -1,14 +1,14 @@
 // Cards.js
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faCreditCard } from "@fortawesome/free-solid-svg-icons";
 import Footer from "./Footer";
 import AddCard from "./Add_card"; // Ensure the file path is correct
 import "./Cards.css";
 
 function Cards() {
   const [isAddCardOpen, setIsAddCardOpen] = useState(false);
-  const [cards, setCards] = useState([]);  // List of cards
+  const [cards, setCards] = useState([]); // List of cards
 
   const handleAddCardClick = () => {
     setIsAddCardOpen(true); // This function toggles the overlay's visibility
@@ -19,8 +19,8 @@ function Cards() {
   };
 
   const handleAddCard = (card) => {
-    console.log('New card added:', card);  // Verify the received card object
-    setCards(prevCards => [...prevCards, card]);
+    console.log("New card added:", card); // Verify the received card object
+    setCards((prevCards) => [...prevCards, card]);
     setIsAddCardOpen(false); // Close the form after adding
   };
 
@@ -34,10 +34,14 @@ function Cards() {
         </button>
         {cards.map((card, index) => (
           <div key={index} className="card-entry">
-            Bank: {card.bank} - Card: {card.cardType}
+            <FontAwesomeIcon icon={faCreditCard} /> {card.bank} {card.cardType}
           </div>
         ))}
-        <AddCard isOpen={isAddCardOpen} onClose={handleClose} onSubmit={handleAddCard} />
+        <AddCard
+          isOpen={isAddCardOpen}
+          onClose={handleClose}
+          onSubmit={handleAddCard}
+        />
       </div>
       <Footer />
     </div>
