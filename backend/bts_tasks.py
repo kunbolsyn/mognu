3d6c7458877974af6d803ff5263ffb843831d60a
AnsarYesma <ansar.yesmukhanov@gmail.com>
2024-04-14 10:58:13 +0500

    upd

--- backend/bts_tasks.py
+++ backend/bts_tasks.py
@@ -11,6 +11,7 @@ import json
 import re
 import utils
 from celery.schedules import crontab
+import openai
 
 class BaseTaskWithRetry(Task):
     autoretry_for = (Exception,)
@@ -112,17 +113,54 @@ def checklink(url):
 @app.task(bind=True, ignore_result=True)
 def queueing(self, ):
     redis_client.flushdb()
-    for url in [
-        "https://guide.kaspi.kz/client/ru",
+    for url, bank in [
+        ("https://guide.kaspi.kz/client/ru", "kaspi"),
+        # ('https://jusan.kz/faq/bank/cashback-bonus/bon-prog/2459', 'jusan')
     ]:
         redis_client.rpush('urls', lzma.compress(pickle.dumps((url, 0))))
         redis_client.execute_command("BF.ADD %s %s" % (bloom_filter_key, url))
-    while (redis_client.scard('current')>0 or redis_client.llen('urls')>0):
-        if redis_client.llen('urls') > 0:
-            url, depth = pickle.loads(lzma.decompress(redis_client.lpop('urls')))
-            if checklink(url):
-                redis_client.sadd('current', url)
-                search.apply_async((url, depth), compression='brotli', priority=3)
+        while (redis_client.scard('current')>0 or redis_client.llen('urls')>0):
+            if redis_client.llen('urls') > 0:
+                url, depth = pickle.loads(lzma.decompress(redis_client.lpop('urls')))
+                if checklink(url):
+                    redis_client.sadd('current', url)
+                    search.apply_async((url, depth), compression='brotli', priority=3)
+        logger.info(f"{bank} DONE")
+        prompt(bank)
+        redis_client.delete('all_text')
+
+def prompt(bank):
+    all_text = ''
+    while (redis_client.llen('all_text') > 0):
+        x = pickle.loads(lzma.decompress(redis_client.lpop('all_text')))
+        print(x)
+        all_text += x
+        all_text += '\n'
+    logger.info(f"{bank} CASH")
+    with open(f'{bank}_cashbacks.txt', 'w', encoding='utf-8') as f:
+        f.write(all_text)
+        # json.dump(all_text, f, ensure_ascii=False, indent=4)
+    messages = [ {"role": "system", "content":  
+              "You will be given a russian FAQ page about a cashback system of {bank} Bank. FAQ may be poorly structured. If cashback category is undefined, make a copies of this cashback with all categories. Your task is to parse information in form of json file. "},
+              {"role": "system", "content":""" this is the example json:
+              {[
+                {"bank": "Jusan",
+                "category":"Такси",
+                "category_mcc": "4121",
+                "category_description":"Услуги по транспортировке пассажиров в автомобилях и службах такси.",
+                "cashback_offers": [
+                    {
+                        "requirement": "",
+                        "cashback_value": "7%"
+                    },
+                    {
+                        "requirement": "Elite card",
+                        "cashback_value": "15%"
+                    }
+                ]}
+              ]}
+              """}]
+
 
 @app.task(bind=True, ignore_result=True, base=BaseTaskWithRetry)
 def search(self, url, depth):
@@ -137,6 +175,7 @@ def search(self, url, depth):
     logger.info(f"{depth} on {url}")
     recurse(soup, url)
     if (depth == 3):
+        redis_client.srem('current', url)
         return 1
     for a in soup.select('a'):
         link = a.get('href')
@@ -158,7 +197,7 @@ def recurse(soup, url):
             sentences = re.split(r'\n+|\s{2,}', div_text)
             for sentence in sentences:
                 if sentence and not (sentence == "" or sentence == "/n" or sentence == " "):
-                    redis_client.rpush('all_text', sentence.strip())
+                    redis_client.rpush('all_text', lzma.compress(pickle.dumps(sentence.strip())))
     return
 
 # import pymorphy2
